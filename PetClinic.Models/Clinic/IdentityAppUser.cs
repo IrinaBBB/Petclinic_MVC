0a8825f1a67353fc9605ac31f8558633ea602f8d
IrinaBBB <balkarovairina@gmail.com>
2023-10-13 22:55:26 +0200

    Add UI List for Users

--- PetClinic.Models/Clinic/IdentityAppUser.cs
+++ PetClinic.Models/Clinic/IdentityAppUser.cs
@@ -1,25 +1,19 @@
 ﻿#nullable enable
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace PetClinic.Models.Clinic
 {
     public class IdentityAppUser : IdentityUser
     {
-        [Required]
-        [StringLength(128)]
-        public string FirstName { get; set; }
-
-        [Required]
-        [StringLength(128)]
-        public string LastName { get; set; }
-
+        [Required] [StringLength(128)] public string FirstName { get; set; }
+        [Required] [StringLength(128)] public string LastName { get; set; }
         public string? StreetAddress { get; set; }
-
         public string? City { get; set; }
-
         public string? State { get; set; }
-
         public string? PostalCode { get; set; }
+        [NotMapped] public string? RoleId { get; set; }
+        [NotMapped] public string? Role { get; set; }
     }
 }
--- Petclinic/Areas/Admin/Controllers/UserController.cs
+++ Petclinic/Areas/Admin/Controllers/UserController.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using PetClinic.Models.Shop;
-using Petclinic.Repository.IRepository;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PetClinic.DataAccess;
+using Microsoft.AspNetCore.Identity;
+using PetClinic.Models.Clinic;
+using System.Linq;
 
 namespace Petclinic.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class UserController : Controller
     {
-        private readonly IUnitOfWork _db;
+        private readonly IdentityContext _db;
+        private readonly UserManager<IdentityAppUser> _userManager;
 
-        public UserController(IUnitOfWork db)
+        public UserController(IdentityContext db, UserManager<IdentityAppUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var userList = _db.IdentityUsers.ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in userList)
+            {
+                var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+                user.Role = role == null ? "None" : roles.FirstOrDefault(u => u.Id == role.RoleId)!.Name;
+            }
+            return View(userList);
         }
 
         private void ReturnTempMessage(string type, string message)
