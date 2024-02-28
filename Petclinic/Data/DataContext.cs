fcebb005a3edb720b97f754784467abf01ca78b6
IrinaB <balkarovairina@gmail.com>
2024-02-28 07:49:10 +0100

    Refactor Remove Tables from DB and Old Migrations

--- Petclinic/Data/DataContext.cs
+++ Petclinic/Data/DataContext.cs
@@ -8,13 +8,8 @@ namespace Petclinic.Data
     {
         public DataContext(DbContextOptions options) : base(options) { }
 
-        public DbSet<Specialty> Specialties { get; set; }
-        public DbSet<Owner> Owners { get; set; }
-        public DbSet<Vet> Vets { get; set; }
-        public DbSet<PetType> PetTypes { get; set; }
-        public DbSet<Pet> Pets { get; set; }
-        public DbSet<Visit> Visits { get; set; }
-
+        
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) { }
     }
